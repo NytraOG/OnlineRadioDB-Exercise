10af2cfd2c1ff3b2622dc00492d12eebce3d4e9b
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-18 11:37:55 +0100

    songtest added

---
+++ OnlineRadioDB/ClassLibrary/Song.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Song
+    {
+        private string songName;
+        private string artistName;
+        private TimeSpan songLength;
+
+        public Song(string songName, string artistName)
+        {
+            SongName = songName;
+            ArtistName = artistName;
+        }
+
+        public string SongName
+        {
+            get => songName;
+            set
+            {
+                try
+                {
+                    if (value.Length < 30 && value.Length > 3)
+                    {
+                        songName = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Invalid Songname. Try one with 3 to 30 characters!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        public string ArtistName
+        {
+            get => artistName;
+            set
+            {
+                try
+                {
+                    if (value.Length > 2 && value.Length < 21)
+                    {
+                        artistName = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Invalid Artistname. Try one with 3 to 20 characters!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        public TimeSpan SongLength
+        {
+            get => songLength;
+            set
+            {
+
+            }
+        }
+    }
+}
---
+++ OnlineRadioDB/Tests/SongTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exception = System.Exception;
+
+namespace Tests
+{
+    [TestClass]
+    public class SongTests
+    {
+        [TestMethod]
+        public void ShouldThrowExceptionIfTooManyCharactersInArtistName()
+        {
+            //Arrange
+            const string artistNameInput = "Hermantha Wickramaratne";
+            const string songName = "Der Blubbersong";
+
+            //Act & Assert
+            Assert.ThrowsException<Exception>(() => new Song(songName, artistNameInput));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfTooManyCharactersInSongName()
+        {
+            //Arrange
+            const string songNameInput = "It's The End Of The World As We Know It (And I Feel Fine)";
+            const string artistname = "Hermann Holts";
+
+            //Act & Assert
+            Assert.ThrowsException<Exception>(() => new Song(songNameInput, artistname));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfNotEnoughCharactersInSongName()
+        {
+            //Arrange
+            const string songNameInput = "A";
+            const string artistName = "Hermann Holts";
+
+            //Act & Assert
+            Assert.ThrowsException<Exception>(() => new Song(songNameInput, artistName));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfNotEnoughCharactersInArtistName()
+        {
+            //Arrange
+            const string artistNameInput = "I";
+            const string songName = "Ein Lied";
+
+            //Act & Assert
+            Assert.ThrowsException<Exception>(() => new Song(songName, artistNameInput));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfArtistIsNullOrEmpty()
+        {
+            //Arrange
+            const string artistNameInput = "";
+            const string songname = "Der Blubbersong";
+
+            //Act & Assert
+            Assert.ThrowsException<Exception>(() => new Song(songname, artistNameInput));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfSongIsNullOrEmpty()
+        {
+            //Arrange
+            const string songNameinput = "";
+            const string artistName = "Hermann Holts";
+
+            //Act & Assert
+            Assert.ThrowsException<Exception>(() => new Song(songNameinput, artistName));
+        }
+    }
+}
